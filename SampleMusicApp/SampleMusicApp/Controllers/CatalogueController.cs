643b5e80109def1409a93da9f2cc8cfeb16e77b7
Shawn Beachy <Shawn Beachy>
2016-08-19 15:23:24 -0400

    Add ignored file. Explicitly tell it to get DateCreated.

--- SampleMusicApp/SampleMusicApp/Controllers/CatalogueController.cs
+++ SampleMusicApp/SampleMusicApp/Controllers/CatalogueController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.Storage.FileProperties;
 using Windows.Storage.Search;
 
 namespace SampleMusicApp.Controllers
@@ -12,10 +14,22 @@ namespace SampleMusicApp.Controllers
             //Get the Music library folder.
             var musicFolder = KnownFolders.MusicLibrary;
 
-            //Get a list of StorageFolders grouped by the System.Music.AlbumName property.
-            var albumFolders = await musicFolder.GetFoldersAsync(CommonFolderQuery.GroupByAlbum);
+            //Create a new query which will group folders by the System.Music.AlbumName property.
+            var query = new QueryOptions(CommonFolderQuery.GroupByAlbum);
 
-            //Get a list of all StorageFolders in the music library.
+            //Try to explicitly tell it to fetch the DateCreated property since it wasn't by default.
+            query.SetPropertyPrefetch(PropertyPrefetchOptions.MusicProperties, new List<string>
+            {
+                "System.DateCreated"
+            });
+
+            //Query subfolders.
+            query.FolderDepth = FolderDepth.Deep;
+
+            //Get a list of StorageFolders grouped by the query.
+            var albumFolders = await KnownFolders.MusicLibrary.CreateFolderQueryWithOptions(query).GetFoldersAsync();
+
+            //Get a list of all StorageFolders in the music library (for comparison).
             var allFolders = await musicFolder.GetFoldersAsync();
 
             foreach (var albumFolder in albumFolders)
@@ -23,13 +37,13 @@ namespace SampleMusicApp.Controllers
                 //Path is empty
                 var folderPath = albumFolder.Path;
 
-                //DateCreated isn't set (so Windows returns 12/31/1600).----------------------------------
-                var creationDate = albumFolder.DateCreated; //                                           |
-                //                                                                                       |
-                //...which wouldn't be so bad if this property would be set. But it's always 0. <---------
+                //DateCreated isn't set (so Windows returns 12/31/1600 7:00:00).----------------------------------
+                var creationDate = albumFolder.DateCreated; //                                                   |
+                //                                                                                               |
+                //...which wouldn't be so bad if this property would be set. But it's always 0. <-----------------
                 var releaseYear = (await albumFolder.Properties.GetMusicPropertiesAsync()).Year;
 
-                //NOTE: Occasionally one or both of these properties *will* be set correctly. I can't find any rhyme or reason to this.
+                //NOTE: Occasionally one or both of these properties *will* be set correctly. I can't find any rhyme or reason for this.
             }
 
             foreach (var folder in allFolders)
